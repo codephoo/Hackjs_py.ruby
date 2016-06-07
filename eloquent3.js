834b4f0271bb637583c887a40ea0ad62ca01e4d8
Shola Abaogun <admin@codedgeek.com>
2016-06-07 12:19:21 +0100

    solve second exercise

--- eloquent3.js
+++ eloquent3.js
@@ -1,5 +1,6 @@
 //first exercise in chapter 3 of eloquent
 'use strict'
-min= (x,y) =>	(x<y) ? x: y;
+min= (x,y) => (x<y) ? x: y;
 
-
+//second exercise of chapter 3
+isEven = number => number%2 == 0 ? true : false;
