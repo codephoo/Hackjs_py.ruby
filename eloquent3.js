ba71530914341e466fdedd9a2f4d809d89b59fab
Shola Abaogun <admin@codedgeek.com>
2016-06-07 12:50:28 +0100

    add count B's function

--- eloquent3.js
+++ eloquent3.js
@@ -3,4 +3,15 @@
 min= (x,y) => (x<y) ? x: y;
 
 //second exercise of chapter 3
-isEven = number => number%2 == 0 ? true : false;
+isEven = number => number%2 === 0 ? true : false;
+
+//Bean Counting
+countBs = string => {
+  string = String(string);
+  let count = 0
+  for (var i = string.length - 1; i >= 0; i--) {
+    if (string[i]==='B')
+      count++;
+  };
+  return count;
+}
