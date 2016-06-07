7168d8803eac7ed14b5c5befd5cdb9c6101c558a
Shola Abaogun <admin@codedgeek.com>
2016-06-07 12:54:19 +0100

    add countchar function

--- eloquent3.js
+++ eloquent3.js
@@ -5,12 +5,24 @@ min= (x,y) => (x<y) ? x: y;
 //second exercise of chapter 3
 isEven = number => number%2 === 0 ? true : false;
 
-//Bean Counting
+//Bean Counting, count B's
 countBs = string => {
   string = String(string);
   let count = 0
   for (var i = string.length - 1; i >= 0; i--) {
     if (string[i]==='B')
+      count++;
+  };
+  return count;
+}
+
+//Bean Counting, count char
+countChar = (string, character) => {
+  string = String(string);
+  character = String(character);
+  let count = 0
+  for (var i = string.length - 1; i >= 0; i--) {
+    if (string[i]===character)
       count++;
   };
   return count;
